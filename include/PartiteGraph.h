bab78b3a7dfcf87dc6dcf4e0303ccfa7c4eb8a63
Liam Childs <Liam Childs>
2017-07-20 15:41:55 +0200

    added prototype add_vertex

--- include/PartiteGraph.h
+++ include/PartiteGraph.h
@@ -7,86 +7,58 @@
 #include "Graph.h"
 
 
-template<typename V, std::size_t N>
+template<typename V, typename ...Ts>
 class PartiteGraph : public Graph<V> {
 public:
-
-    /**
-     * Add an edge between two named vertices
-     * @param from vertex to join from
-     * @param to vertex to join to
-     */
-    void add_edge(const V &from, const V &to) {
-        int from_partition = _get_partition(from);
-        int to_partition = _get_partition(to);
-        if (from_partition == -1) {
-            std::stringstream message;
-            message << "Vertex " << from << " has not been assigned a partition.";
-            throw std::runtime_error(message.str());
-        }
-        else if (to_partition == -1) {
-            std::stringstream message;
-            message << "Vertex " << to << " has not been assigned a partition.";
-            throw std::runtime_error(message.str());
-        }
-        else if (from_partition == to_partition) {
-            std::stringstream message;
-            message << "Vertex " << from << " and vertex " << to <<
-                    " can not be connected as they are both in partition " << from_partition << ".";
-            throw std::runtime_error(message.str());
-        }
-        Graph<V>::add_edge(from, to);
-    }
-
     void add_vertex(const V &vertex) = delete;
 
-    /**
-     * Add a named vertex
-     * @param vertex name of the vertex to add
-     * @param partition partition to add vertex to
-     */
-    void add_vertex(const V &vertex, int partition) {
-        int current_partition = _get_partition(vertex);
-        if (current_partition == -1) {
-            partitions[partition].insert(vertex);
-            Graph<V>::add_vertex(vertex);
-        }
-        else if (current_partition != partition) {
-            std::stringstream message;
-            message << "Vertex " << vertex << " already exists in partition " << current_partition << ".";
-            throw std::runtime_error(message.str());
-        }
-    }
+    /*template<typename T>
+    T &get_vertex(const V &vertex) {
+        std::stringstream message;
+        message << "Vertex " << vertex << " not found.";
+        throw std::runtime_error(message.str());
+    }*/
 
-    /**
-     * Join two graphs
-     * @param that graph to join to
-     */
-    void update(const PartiteGraph<V, N> &that) {
-        for (unsigned int i = 0; i < N; ++i) {
-            partitions[i].insert(that.partitions[i].begin(), that.partitions[i].end());
-        }
+    /*void update(const PartiteGraph<V, Ts...> &that) {
         Graph<V>::update(that);
-    }
-
-    /**
-     * Get the partition of a vertex. -1 if vertex not in a partition.
-     * @param vertex get the partition of this vertex
-     * @return partition the vertex belongs to
-     */
-    int get_partition(const V &vertex) {
-        for (unsigned int i = 0; i < N; ++i) {
-            if (partitions[i].find(vertex) != partitions[i].end()) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-private:
-
-    std::array<std::unordered_set<V>, N> partitions;
-
+    }*/
 };
+
+template<typename V, typename T, typename ...Ts>
+class PartiteGraph<V, T, Ts...> : public PartiteGraph<V, Ts...> {
+public:
+    std::unordered_map<V, T> _partition;
+};
+
+
+template <unsigned int, typename> struct VertexAdder;
+
+template <class V, class T, class... Ts>
+struct VertexAdder<0, PartiteGraph<V, T, Ts...>> {
+    typedef T type;
+};
+
+template <unsigned int partition, class V, class T, class... Ts>
+struct VertexAdder<partition, PartiteGraph<V, T, Ts...>> {
+    typedef typename VertexAdder<partition - 1, PartiteGraph<V, Ts...>>::type type;
+};
+
+
+/**
+ * Add a named vertex
+ * @param vertex name of the vertex to add
+ * @param partition partition to add vertex to
+ */
+template <unsigned int partition, class V, class... Ts>
+void add_vertex(PartiteGraph<V, Ts...>& graph, const V &vertex, typename std::enable_if<partition == 0, const typename VertexAdder<0, PartiteGraph<V, Ts...>>::type>::type &type) {
+    graph._partition.emplace(vertex, type);
+}
+
+template <unsigned int partition, class V, class T, class... Ts>
+void add_vertex(PartiteGraph<V, T, Ts...>& graph, const V &vertex, typename std::enable_if<partition != 0, const typename VertexAdder<partition, PartiteGraph<V, T, Ts...>>::type>::type &type) {
+    PartiteGraph<V, Ts...>& base = graph;
+    return add_vertex<partition - 1>(base, vertex, type);
+}
+
 
 #endif //GRAPH_PARTITEGRAPH_H
--- tests/test_PartiteGraph.cpp
+++ tests/test_PartiteGraph.cpp
@@ -5,25 +5,32 @@
 #include "PartiteGraph.h"
 
 
-TEST(PartiteGraphTest, test_add_vertex) {
-    PartiteGraph<int, 2> graph;
+struct Vertex {
+    std::string name;
+};
 
-    graph.add_vertex(0, 0);
-    EXPECT_THROW(graph.add_vertex(0, 1), std::runtime_error);
+struct Edge {
+    float weight;
+};
+
+TEST(PartiteGraphTest, test_add_vertex) {
+    PartiteGraph<int, Vertex, Edge> graph;
+
+    add_vertex<0>(graph, 0, Vertex{"vertex 0"});
 }
 
 TEST(PartiteGraphTest, test_add_edge) {
-    PartiteGraph<int, 2> graph;
+    PartiteGraph<int, Vertex, Edge> graph;
 
     EXPECT_THROW(graph.add_edge(0, 1), std::runtime_error);
 
-    graph.add_vertex(0, 0);
-    graph.add_vertex(1, 0);
+    add_vertex<0>(graph, 0, Vertex{"vertex 0"});
+    add_vertex<0>(graph, 1, Vertex{"vertex 1"});
     EXPECT_THROW(graph.add_edge(0, 1), std::runtime_error);
 
-    graph.add_vertex(2, 1);
-    graph.add_edge(0, 2);
-    graph.add_edge(1, 2);
+    add_vertex<1>(graph, 0, Edge{0.1});
+    /*add_edge(0, 2);
+    add_edge(1, 2);
     std::unordered_set<int> expected{0, 1};
-    EXPECT_EQ(graph.get_parents(2), expected);
+    EXPECT_EQ(graph.get_parents(2), expected);*/
 }
